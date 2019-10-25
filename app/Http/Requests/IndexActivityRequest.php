02975a2ec8db6199e02c7ac37e695334a2c12f43
Tairone Livinalli <tairone.livinalli@meta.com.br>
2019-10-25 16:54:10 -0300

    Adicionado IndexActivityRequest

---
+++ app/Http/Requests/IndexActivityRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class IndexActivityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'initial_date' => 'nullable|required_with:final_date|date_format:Y-m-d H:i|before:final_date|',
+            'final_date'   => 'nullable|required_with:initial_date|date_format:Y-m-d H:i|after:initial_date|',
+        ];
+    }
+
+    /**
+     * Failed validation disable redirect
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
